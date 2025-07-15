d108d4b0906a67093338636ebe5509b557767dce
217eastbroadway <newyorkedoardo@gmail.com>
2025-07-15 21:59:34 +0200

    big big big

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <raylib.h>
 
 class Pad {
     private:
         Sound sound;
-        int posX;
-        int posY;
+        Texture2D baseTexture, hitTexture;
+        int posX, posY, id;
 
     public:
-        Pad(std::string sound, int posX, int posY) {
+        static int padCount;
+
+        Pad(std::string sound, std::string baseTexture, std::string hitTexture, int posX, int posY) {
             this->sound = LoadSound(sound.c_str());
+            this->baseTexture = LoadTextureFromImage(LoadImage(baseTexture.c_str()));
+            this->hitTexture = LoadTextureFromImage(LoadImage(hitTexture.c_str()));
 
             this->posX = posX;
             this->posY = posY;
+            id = padCount;
+            padCount++;
         }
 
         Pad() {
             sound = LoadSound("res/default.wav");
+            baseTexture = LoadTextureFromImage(LoadImage("res/bluebutton.png"));
+            hitTexture = LoadTextureFromImage(LoadImage("res/redbutton.png"));
 
             this->posX = 0;
             this->posY = 0;
+            id = padCount;
+            padCount++;
         }
 
         ~Pad() {
             UnloadSound(sound);
         }
+        
+        int getId() {
+            return id;
+        }
 
-        void changeSound(std::string sound) {
+        Vector2 getPadPosition() {
+            return (Vector2){posX, posY};
+        }        
+
+        void setSound(std::string sound) {
             UnloadSound(this->sound);
             this->sound = LoadSound(sound.c_str());
         }
@@ -35,22 +54,51 @@ class Pad {
         void playSound() {
             PlaySound(sound);
         }
+
+        void renderPad(bool hit) {
+            Rectangle sourceRec = { 0.0f, 0.0f, (float)baseTexture.width, (float)baseTexture.height };
+            Rectangle destRec = {posX, posY, 100.0f, 100.0f };
+            Vector2 origin = { 0.0f, 0.0f };
+
+            DrawTexturePro(hit ? hitTexture : baseTexture, sourceRec, destRec, origin, 0.0f, WHITE);
+        }
 };
 
-int main() {
-    InitWindow(500, 650, "SensEX");
-    InitAudioDevice();
-    Pad p1 = Pad();
+int Pad::padCount = 0;
 
-    Texture2D bluetexture = LoadTextureFromImage(LoadImage("res/bluebutton.png"));
-    Texture2D redtexture = LoadTextureFromImage(LoadImage("res/redbutton.png"));
+int main() {
+    InitWindow(525, 650, "SensEx");
+    InitAudioDevice();
+
+    std::vector<Pad*> pads;
+    pads.push_back(new Pad("res/default.wav", "res/bluebutton.png", "res/redbutton.png", 25, 25));
+    pads.push_back(new Pad("res/kick.wav", "res/bluebutton.png", "res/redbutton.png", 150, 25));
+    pads.push_back(new Pad("res/snare.wav", "res/bluebutton.png", "res/redbutton.png", 275, 25));
+    pads.push_back(new Pad("res/cymbal.wav", "res/bluebutton.png", "res/redbutton.png", 400, 25));
 
     while(!WindowShouldClose()) {
-        if(IsKeyPressed(KEY_ENTER))
-            p1.playSound();
-
         BeginDrawing();
-        DrawTexture(bluetexture, 25, 25, WHITE);
+        ClearBackground(WHITE);
+
+        int i = 0;
+        for(Pad *p : pads) {
+            Vector2 mousePos;
+            Vector2 padPos = p->getPadPosition();
+
+            if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                std::cout << "<sensex::input> MOUSE_LEFT pressed." << std::endl;
+                mousePos = GetMousePosition();
+                DrawRectangle(mousePos.x, mousePos.y, 2, 2, WHITE);
+
+                if((mousePos.x >= padPos.x && mousePos.x <= padPos.x + 100.0f) && (mousePos.x >= padPos.y && mousePos.y <= padPos.y + 100.0f)) {
+                    p->renderPad(true);
+                    p->playSound();
+                }
+            }
+            else
+                p->renderPad(false);
+        }
+
         EndDrawing();
     }
 }
